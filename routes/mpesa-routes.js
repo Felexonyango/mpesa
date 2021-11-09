8b5bfa375c61b47f6b7cd13c320c3eae0cad5003
Felexonyango <felexonyango19@gmail.com>
2021-11-09 20:19:23 +0300

    intial commit

---
+++ routes/mpesa-routes.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+const { mpesaPassword,token,stkPush } = require('../controllers/mpesaController');
+const router = express.Router();
+
+
+
+router.get('/password',mpesaPassword);
+
+router.post('/stkpush',token,stkPush);
+
+
+
+
+module.exports = {
+    routes:router
+}
